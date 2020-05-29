1f4a2ff229cf9a1c81080ea042efc88acbe32264
Kamille Longalong <k.pagibigan@gmail.com>
2020-05-29 00:31:59 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def self.match(word_array)
+    word_array.split("").sort ==
+  end
+  
+  
+end
